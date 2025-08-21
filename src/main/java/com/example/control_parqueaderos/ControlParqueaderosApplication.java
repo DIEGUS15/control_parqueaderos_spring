bde039996fee9a6306050284d384ae948e6c7cd8
DIEGUS15 <diegogranados1501@gmail.com>
2025-08-20 21:35:36 -0500

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/control_parqueaderos/ControlParqueaderosApplication.java
@@ -0,0 +1,13 @@
+package com.example.control_parqueaderos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlParqueaderosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlParqueaderosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/control_parqueaderos/ControlParqueaderosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.control_parqueaderos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlParqueaderosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
